6b970370f911067722c5ca253fd46d605f905336
aimee-liang <aimeeeliang@gmail.com>
2020-10-08 00:52:21 -0400

    Working on deliverable 2

--- src/index.js
+++ src/index.js
@@ -7,38 +7,40 @@ document.addEventListener('DOMContentLoaded', function(){
 
 /* Deliverable #1: */
 
-function returnImages(){
+function returnDogImages(){
     const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
-    // fetch the images using the url above ⬆️
+    // fetch the images using the url above 
     fetch(imgUrl)
     // parse the response as JSON
     .then(response => response.json())
     // add image elements to the DOM for each image in the array
-    // for each image in json, create an element
     .then(image => {
-        image.message.forEach(image => {
-            
-        })
-    })
-    
+        image.message.forEach(image => addDogImages(image))
+    });
 }
 
-function addImage(){
-    // create elements for each image in the API
-    
+// helper function - it creates elements for each image in the API
+function addDogImages(imagePlaceholder){
+    // create variable assigned to value of querying doc for an element
+    const dogImageContainer = document.querySelector("#dog-image-container");
+    // create an element for this image
+    let dogImageTag = document.createElement("img");
+    // assign the value of this new image src to the image in func above
+    dogImageTag.src = imagePlaceholder;
+    // append this to the dogImageContainer
+    dogImageContainer.appendChild(dogImageTag);
 }
-
 
 
 /* Deliverable 2 */
 
 function getDogs(){
     const breedUrl = 'https://dog.ceo/api/breeds/list/all'
-    // on page load, fetch all the dog breeds using the url above ⬆️
+    // on page load, fetch all the dog breeds using the url above 
     fetch(breedUrl)
-    
+        .then(response => response.json());
     // add the breeds to the page in an <ul> (take a look at the included index.html) by append
-    .then
+        .then()
 }
 
 /* Deliverable 3 */
