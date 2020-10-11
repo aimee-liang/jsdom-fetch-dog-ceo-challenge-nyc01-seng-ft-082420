cbe02512461881badeb17f56e8d38d9f923555d3
aimee-liang <aimeeeliang@gmail.com>
2020-10-11 00:21:47 -0400

    Working on Deliverable 3

--- src/index.js
+++ src/index.js
@@ -1,55 +1,69 @@
 
-
 document.addEventListener('DOMContentLoaded', function(){
     returnImages();    
     
-})
-
-/* Deliverable #1: */
-
-function returnDogImages(){
-    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
-    // fetch the images using the url above 
-    fetch(imgUrl)
-    // parse the response as JSON
-    .then(response => response.json())
-    // add image elements to the DOM for each image in the array
-    .then(image => {
-        image.message.forEach(image => addDogImages(image))
-    });
-}
-
-// helper function - it creates elements for each image in the API
-function addDogImages(imagePlaceholder){
-    // create variable assigned to value of querying doc for an element
-    const dogImageContainer = document.querySelector("#dog-image-container");
-    // create an element for this image
-    let dogImageTag = document.createElement("img");
-    // assign the value of this new image src to the image in func above
-    dogImageTag.src = imagePlaceholder;
-    // append this to the dogImageContainer
-    dogImageContainer.appendChild(dogImageTag);
-}
-
-
-/* Deliverable 2 */
-
-function getDogs(){
-    const breedUrl = 'https://dog.ceo/api/breeds/list/all'
-    // on page load, fetch all the dog breeds using the url above 
-    fetch(breedUrl)
+    
+    /* Deliverable #1: */
+    const returnDogImages = () => {
+        const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+        fetch(imgUrl)
+        .then(response => response.json())
+        .then(image => {
+            image.message.forEach(image => addDogImages(image))
+        });
+    }
+    
+    // helper function - it creates elements for each image in the API
+    const addDogImages = (imagePlaceholder) => {
+        let dogImageContainer = document.querySelector("#dog-image-container");
+        let dogImageTag = document.createElement("img");
+        dogImageTag.src = imagePlaceholder;
+        dogImageContainer.appendChild(dogImageTag);
+    }
+    
+    
+    /* Deliverable 2 */
+    
+    function getDogs(){
+        const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+        fetch(breedUrl)
         .then(response => response.json());
-    // add the breeds to the page in an <ul> (take a look at the included index.html) by append
-        .then()
-}
+        .then(results => {
+            breeds = Object.keys(results.message);
+            createDogBreedList(breeds);
+            // addBreedSelectListener
+        })
+    }
+    
+    // function creates ul and adds list to ul
+    function createDogBreedList(breeds){
+        let unList = document.querySelector("#dog-breeds");
+        let dogBreedList = document.createElement("li");
+        unList.append(dogBreedList);
+    }
+    
+    
+    /* Deliverable 3 */
+    // add event listener
+    function fontChange(){
+        // font color of a particular <li> changes on click.
+        document.addEventListener('click', e => {
+            if (e.target.matches("select-breed")){
+                
+            }
+        })
+        
+        // When the user clicks any of the dog breed list items, the color the text should change.
+    }
+    
+    
+    
+    /* Deliverable 4 */
+    
+    // Once we are able to load all of the dog breeds onto the page, add JavaScript so that the user can filter breeds that start with a particular letter using a dropdown.
+    
+    
 
-/* Deliverable 3 */
+    
 
-// Once all of the breeds are rendered in the <ul>, add JavaScript so that the font color of a particular <li> changes on click. This can be a color of your choosing.
-
-// When the user clicks any of the dog breed list items, the color the text should change.
-
-/* Deliverable 4 */
-
-// Once we are able to load all of the dog breeds onto the page, add JavaScript so that the user can filter breeds that start with a particular letter using a dropdown.
-
+})
